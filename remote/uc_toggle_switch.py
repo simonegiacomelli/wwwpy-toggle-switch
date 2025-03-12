2ec173214a0210d6f21f81bd43ebc68249f16769
simone <simone.giacomelli@gmail.com>
2025-03-12 16:51:36 +0000

    refactor: update toggle switch styles to use CSS variables for better customization

--- remote/uc_toggle_switch.py
+++ remote/uc_toggle_switch.py
@@ -12,79 +12,77 @@ class UcToggleSwitchComponent(wpc.Component, tag_name='uc-toggle-switch'):
         self.element.attachShadow(dict_to_js({'mode': 'open'}))
         # language=html
         self.element.shadowRoot.innerHTML = """
-<style>
-/* The switch - the box around the slider */
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
-
-/* Hide default HTML checkbox */
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-/* The slider */
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-input:checked + .slider {
-  background-color: #2196F3;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
-</style>        
-        <!-- Rectangular switch -->
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider"></span>
-</label>
-
-<!-- Rounded switch -->
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label>
-"""
+        <style>
+        :host {
+          --width: 3.75em;
+          --height: 2.125em;
+          --offset: 0.25em;
+          --slider-size: calc(var(--height) - 0.5em);
+        }
+        
+        .switch {
+          position: relative;
+          display: inline-block;
+          width: var(--width);
+          height: var(--height);
+        }
+        
+        .switch input {
+          opacity: 0;
+          width: 0;
+          height: 0;
+        }
+        
+        .slider {
+          position: absolute;
+          cursor: pointer;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: #ccc;
+          transition: .4s;
+        }
+        
+        .slider:before {
+          position: absolute;
+          content: "";
+          height: var(--slider-size);
+          width: var(--slider-size);
+          left: var(--offset);
+          bottom: var(--offset);
+          background-color: white;
+          transition: .4s;
+        }
+        
+        input:checked + .slider {
+          background-color: #2196F3;
+        }
+        
+        input:focus + .slider {
+          box-shadow: 0 0 1px #2196F3;
+        }
+        
+        input:checked + .slider:before {
+          transform: translateX(calc(var(--width) - var(--height)));
+        }
+        
+        .slider.round {
+          border-radius: var(--height);
+        }
+        
+        .slider.round:before {
+          border-radius: 50%;
+        }
+        </style>
+        
+        <label class="switch">
+          <input type="checkbox">
+          <span class="slider"></span>
+        </label>
+        
+        <label class="switch">
+          <input type="checkbox">
+          <span class="slider round"></span>
+        </label>
+        """
