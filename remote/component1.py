e3be2c3509d494732532ee32ddd2095d0f66d439
simone <simone.giacomelli@gmail.com>
2025-03-13 16:36:25 +0000

    feat: enhance UcToggleSwitchComponent with text attribute and update Component1 interactions

--- remote/component1.py
+++ remote/component1.py
@@ -4,12 +4,16 @@ import js
 
 import logging
 
+from remote.uc_toggle_switch import UcToggleSwitchComponent
+
 logger = logging.getLogger(__name__)
 
 class Component1(wpc.Component, tag_name='component-1'):
     input1: js.HTMLInputElement = wpc.element()
     slSwitch1: js.HTMLElement = wpc.element()
     button1: js.HTMLButtonElement = wpc.element()
+    sw1: UcToggleSwitchComponent = wpc.element()
+    sw2: UcToggleSwitchComponent = wpc.element()
     def init_component(self):
         # language=html
         self.element.innerHTML = """
@@ -19,15 +23,18 @@ class Component1(wpc.Component, tag_name='component-1'):
 <sl-switch data-name="slSwitch1">slSwitch1</sl-switch>
 <button data-name="button1">button1</button>
 <hr>
-<uc-toggle-switch data-name='sw1' checked></uc-toggle-switch>
+<uc-toggle-switch data-name='sw1' checked text='ccc'></uc-toggle-switch>
 <hr>
-<uc-toggle-switch data-name='sw2'></uc-toggle-switch>
+<uc-toggle-switch data-name='sw2' text='changes the above'></uc-toggle-switch>
 """
     
     async def sw1__input(self, event):
-        logger.debug(f'{inspect.currentframe().f_code.co_name} event fired %s', event)
+        logger.debug(f'sw1={self.sw1.checked_bool} ')
 
-        
+    async def sw2__input(self, event):
+        logger.debug(f'sw2={self.sw1.checked_bool} ')
+        self.sw1.checked_bool = self.sw2.checked_bool
+
     
     async def button1__click(self, event):
         logger.debug(f'{inspect.currentframe().f_code.co_name} event fired %s', event)
--- remote/uc_toggle_switch.py
+++ remote/uc_toggle_switch.py
@@ -1,5 +1,5 @@
+import inspect
 import logging
-
 import js
 import wwwpy.remote.component as wpc
 from wwwpy.remote import dict_to_js
@@ -9,11 +9,12 @@ logger = logging.getLogger(__name__)
 
 class UcToggleSwitchComponent(wpc.Component, tag_name='uc-toggle-switch'):
     checked: str = wpc.attribute()
+    text: str = wpc.attribute()
     _checkbox: js.HTMLInputElement = wpc.element()
+    _text: js.HTMLElement = wpc.element()
 
     def init_component(self):
         self.element.attachShadow(dict_to_js({'mode': 'open'}))
-        # language=html
         self.element.shadowRoot.innerHTML = """<style>
     :host {
         --width: 2.5em;
@@ -21,20 +22,17 @@ class UcToggleSwitchComponent(wpc.Component, tag_name='uc-toggle-switch'):
         --offset: 0.17em;
         --slider-size: calc(var(--height) - 0.33em);
     }
-
     .switch {
         position: relative;
         display: inline-block;
         width: var(--width);
         height: var(--height);
     }
-
     .switch input {
         opacity: 0;
         width: 0;
         height: 0;
     }
-
     .slider {
         position: absolute;
         cursor: pointer;
@@ -45,7 +43,6 @@ class UcToggleSwitchComponent(wpc.Component, tag_name='uc-toggle-switch'):
         background-color: #ccc;
         transition: .4s;
     }
-
     .slider:before {
         position: absolute;
         content: "";
@@ -56,44 +53,58 @@ class UcToggleSwitchComponent(wpc.Component, tag_name='uc-toggle-switch'):
         background-color: white;
         transition: .4s;
     }
-
     input:checked + .slider {
         background-color: #2196F3;
     }
-
     input:focus + .slider {
         box-shadow: 0 0 1px #2196F3;
     }
-
     input:checked + .slider:before {
         transform: translateX(calc(var(--width) - var(--height)));
     }
-
     .slider.round {
         border-radius: var(--height);
     }
-
     .slider.round:before {
         border-radius: 50%;
     }
+    .toggle-text {
+        margin-left: 0.5em;
+        vertical-align: middle;
+        font-size: 1em;
+    }
 </style>
-<!--
-<label class="switch">
-    <input type="checkbox">
-    <span class="slider"></span>
-</label>
--->
-
 <label class="switch">
     <input type="checkbox" data-name="_checkbox">
     <span class="slider round"></span>
 </label>
-        """
-
-        self._update_attributes()
+<span class="toggle-text" data-name="_text"></span>
+"""
+        self._read_attribute_changed()
 
     def attributeChangedCallback(self, name: str, oldValue: str, newValue: str):
-        self._update_attributes()
+        self._read_attribute_changed()
 
-    def _update_attributes(self):
-        self._checkbox.checked = self.checked is not None
+    def _read_attribute_changed(self):
+        self._checkbox.checked = self.checked_bool
+        self._text.textContent = self.text if self.text is not None else ""
+
+    def _checkbox__change(self, event):
+        self.checked_bool = self._checkbox.checked
+
+    def _text__click(self, event):
+        self.checked_bool = not self.checked_bool
+        self._read_attribute_changed()
+        for event_name in ['change', 'input']:
+            self.element.dispatchEvent(js.Event.new(event_name, dict_to_js({"checked": self._checkbox.checked})))
+
+    @property
+    def checked_bool(self) -> bool:
+        return self.element.hasAttribute('checked')
+
+    @checked_bool.setter
+    def checked_bool(self, value: bool):
+        if value:
+            self.element.setAttribute('checked', '')
+        else:
+            self.element.removeAttribute('checked')
